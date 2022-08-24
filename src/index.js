20c9bf7e36d9cfe60ad4ae301aa9cfed013f88ae
“India1947” <prashantw677@gmail.com>
2022-08-24 21:39:58 +0530

    solution middleware

--- src/index.js
+++ src/index.js
@@ -8,19 +8,30 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Prashant-8497:FZjfI0McE6kHa9II@cluster0.rgvnszv.mongodb.net/Prashant8497?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+// app.use (
+//     function (req, res, next) {
+//         console.log ("inside GLOBAL MW");
+//         next();
+//   }
+//   );
 
+  let assignment = function(req, res, next){
+    let currentDate = new Date()
+    let ip = req.ip
+    let url = req.originalUrl
+
+    console.log(currentDate, ip, url)
+    next()
+  }
+  app.use(assignment)
+
+  
 app.use('/', route);
 
 
